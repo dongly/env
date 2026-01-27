62b15bcb2993e2828572aff711904fd072da14be
dongly <chinky@163.com>
2026-01-27 10:06:22 +0800

    refactor: 调整版本信息显示逻辑，优化欢迎信息展示

--- env.py
+++ env.py
@@ -49,16 +49,19 @@ def show_version():
     print('\033[1;36m===================================================================\033[0m')
     print('Environment Information:')
     print('  - ENV_ROOT: %s' % get_env_root())
-    print('  - PKGS_ROOT: %s' % get_package_root())
-    print('  - BSP_ROOT: %s' % get_bsp_root())
     if rtt_ver != (0, 0, 0):
+        print('  - RTT_ROOT: %s' % get_rtt_root())
+        print('  - PKGS_ROOT: %s' % get_package_root())
+        print('  - BSP_ROOT: %s' % get_bsp_root())
         print('  - RT-Thread Version: %d.%d.%d' % rtt_ver)
     print('\033[1;36m===================================================================\033[0m')
 
-def show_version_warning():
+def show_version_warning(is_show_version=True):
     rtt_ver = get_rtt_verion()
 
     if rtt_ver <= (5, 1, 0) and rtt_ver != (0, 0, 0):
+        if is_show_version:
+            show_version()        
         print('env v2.0 has made the following important changes:')
         print('1. Upgrading Python version from v2 to v3')
         print('2. Replacing kconfig-frontends with Python kconfiglib')
@@ -229,6 +232,12 @@ def exec_arg(arg):
 
 
 def main():
+    if args.version:
+        # -v flag, show welcome message
+        show_version()
+        show_version_warning(False)
+        exit(0)
+
     show_version_warning()
     export_environment_variable()
     init_logger(get_env_root())
@@ -236,11 +245,7 @@ def main():
     parser = init_argparse()
     args = parser.parse_args()
 
-    if args.version:
-        # -v flag, show welcome message
-        show_version()
-        show_version_warning()
-    elif not vars(args):
+    if not vars(args):
         parser.print_help()
     else:
         args.func(args)
