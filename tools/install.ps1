0f0ac604cf664b1d87260f7697ed4b297167b5dd
dongly <chinky@163.com>
2026-01-31 01:21:09 +0800

    fix: set console encoding for proper character display in install.ps1

--- tools/install.ps1
+++ tools/install.ps1
@@ -1,3 +1,7 @@
+chcp 65001 > $null
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+[Console]::InputEncoding = [System.Text.Encoding]::UTF8
+
 # File      : install.ps1
 # This file is part of RT-Thread RTOS
 # COPYRIGHT (C) 2006 - 2026, RT-Thread Development Team
