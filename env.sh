6e48efb4abd96cc732ac9983f603834a67702172
Dongly <chinky@163.com>
2026-01-31 15:49:13 +0800

    fix: correct variable assignment syntax in env.sh

--- env.sh
+++ env.sh
@@ -1,13 +1,16 @@
-# RT-Thread Environment Directory
+#!/bin/bash
 # Can be overridden by $ENV_ROOT environment variable
 : "${ENV_ROOT:=$HOME/.rtenv}"
 
 # Virtual environment directory name
-: "${RT_VENV_DIR:=rt-env}"
+RT_VENV_DIR="$ENV_ROOT/venv/rt-env"
+
+echo "Using RT-Thread environment at: $ENV_ROOT"
+echo "Using RT-Thread virtual environment: $RT_VENV_DIR"
 
 # Activate Python virtual environment
-if [ -f "$ENV_ROOT/$RT_VENV_DIR/bin/activate" ]; then
-    source "$ENV_ROOT/$RT_VENV_DIR/bin/activate"
+if [ -f "$RT_VENV_DIR/bin/activate" ]; then
+    source "$RT_VENV_DIR/bin/activate"
     
     # Show welcome message using rt-env command
     if command -v rt-env &> /dev/null; then
